8a90de1f4033da4a3a0e27b8430bdfaa316f5690
Peter Selinger <selinger@mathstat.dal.ca>
2016-07-03 23:12:28 +0200

    Added some more code to lambda calculus.

--- Lambda.hs
+++ Lambda.hs
@@ -2,10 +2,11 @@
 
 import Nominal
 import Prelude hiding ((.))
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 -- | The type of lambda terms, up to alpha-equivalence.
 data Term = Var Atom | App Term Term | Abs (BindAtom Term)
-          deriving (Show)
 
 -- In an ideal programming language, this instance would be
 -- automatically derived with \"deriving\". We could probably make
@@ -30,6 +31,12 @@ lam = lam_named "x"
 lam_named :: String -> (Term -> Term) -> Term
 lam_named n f = with_fresh_named n (\x -> Abs (x . f (Var x)))
 
+-- | An alternative syntax for 'App'.
+(@@) :: Term -> Term -> Term
+m @@ n = App m n
+
+infixl 9 @@
+
 -- | Substitution. Note that it is capture avoiding!
 -- 'subst' /m/ /x/ /n/ substitutes /m/ for 'Var' /x/ in /n/.
 subst :: Term -> Atom -> Term -> Term
@@ -38,4 +45,67 @@ subst m x (Var y)
   | otherwise = Var y
 subst m x (App t s) = App (subst m x t) (subst m x s)
 subst m x (Abs t) = open t (\y s -> Abs (y . subst m x s))
+
+-- | Function composition, re-defined here because we are hiding '.'
+-- from the "Prelude".
+compose :: (b -> c) -> (a -> b) -> (a -> c)
+compose f g x = f (g x)
+
+-- | Pretty-printing of lambda terms.
+instance Show Term where
+  showsPrec d (Var x) = showsPrec d x
+  showsPrec d (App m n) = showParen (d > 10) $
+    showsPrec 10 m `compose` showString " " `compose` showsPrec 11 n
+  showsPrec d (Abs t) = open_for_printing t $ \x s ->
+    showParen (d > 1) $
+      showString ("λ" ++ show x ++ ".") `compose` showsPrec 1 s
+
+-- | Free variables.
+fv :: Term -> Set Atom
+fv (Var x) = Set.singleton x
+fv (App m n) = fv m `Set.union` fv n
+fv (Abs t) = open t (\x s -> Set.delete x (fv s))
+
+-- | Another equivalent way to define free variables.
+fv' :: Term -> Set Atom
+fv' = support
+
+-- | Beta reduction to normal form.
+reduce :: Term -> Term
+reduce (Var x) = Var x
+reduce (App m n) =
+  case reduce m of
+   Abs t -> open t (\x s -> reduce (subst n x s))
+   m' -> App m' (reduce n)
+reduce (Abs t) = open t (\x s -> Abs (x.reduce s))
+
+-- $ Some example terms
+
+-- | Church numeral zero.
+z :: Term
+z = lam_named "s" $ \s -> lam_named "z" $ \z -> z
+
+-- | Successor of Church numeral.
+s :: Term
+s = lam$ \n -> lam_named "s" $ \s -> lam_named "z" $ \z -> s @@ (n @@ s @@ z)
+
+-- | Addition of Church numerals.
+plus :: Term
+plus = lam$ \n -> lam$ \m -> n @@ s @@ m
+
+-- | Multiplication of Church numerals.
+times :: Term
+times = lam$ \n -> lam$ \m -> n @@ (plus @@ m) @@ z
+
+-- | The Church numeral /n/. This function demonstrates a use of
+-- 'with_fresh' to build lambda terms from names.
+church :: Integer -> Term
+church n =
+  with_fresh_named "s" $ \s ->
+    with_fresh_named "z" $ \z ->
+      Abs (s. Abs (z. aux n (Var s) (Var z)))
+  where
+    aux n s z
+      | n <= 0 = z
+      | otherwise = s @@ (aux (n-1) s z)
 
